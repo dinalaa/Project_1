e1e47ef81c6d1756559bbad9d6824772b420126b
amrabdou28 <amrabdou24@gmail.com>
2020-04-06 23:14:23 +0200

    fur3

--- js/service.js
+++ js/service.js
@@ -1,3 +1,34 @@
+/*-- function scrollAppear() {
+    var serviceText =document.querySelector('.service');
+    var introPosition = serviceText.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight ;
+
+
+
+    if( introPosition < screenPosition){
+        serviceText.classList.add('service-appear');
+    }
+}
+
+window.addEventListener(scroll,scrollAppear);
+
+
+
+function scrollAppear() {
+    var designText =document.querySelector('.design');
+    var introPosition = designText.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight;
+
+
+
+    if( introPosition < screenPosition){
+        designText.classList.add('design-appear');
+    }
+}
+
+window.addEventListener(scroll,scrollAppear); --*/
+
+
 
 //form
 var myInput =  document.getElementById('mad')
